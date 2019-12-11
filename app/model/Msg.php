abc2c5cce375ed9ed6985e32e948b311bd03af19
suconghou <1126045770@qq.com>
2019-12-11 21:09:28 +0800

    improve

---
+++ app/model/Msg.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Msg extends db
+{
+
+
+    static function t(string $t)
+    {
+        return "message_{$t}";
+    }
+
+    static function create(string $t, array $data)
+    {
+        $table = self::t($t);
+        return self::insert($data, $table);
+    }
+
+    static function getList(string $table, string $ns, string $thread)
+    {
+        $where = [
+            'ns' => $ns,
+            'thread' => $thread,
+        ];
+        $table = self::t($table);
+        $orderLimit = [];
+        $data = self::find($where, $table, '*', $orderLimit);
+        return $data;
+    }
+}
